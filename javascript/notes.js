e022896e03bae80a5e665e576b9ca65a95fb642c
Reuben Brandt <Reubennbrandt@gmail.com>
2016-07-01 22:40:15 -0700

    Add buttons

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,10 +1,24 @@
 var Note = React.createClass({
+    edit: function(){
+      alert('editing note');
+    },
+    remove: function(){
+      alert('removing note');
+    },
     render: function() {
       return (
-        <div className="note"></div>
+        <div className="note">
+          <p>{this.props.children}</p>
+          <span>
+            <button onClick={this.edit}
+             className="btn btn-primary glyphicon-pencil"/>
+            <button onClick={this.remove}
+            className="btn btn-danger glyphicon-trash"/>
+          </span>
+        </div>
         );
     }
 });
 
-React.render(<Note>Hello World</Note>,
+React.render(<Note>Note Here</Note>,
     document.getElementById('react-container'));
