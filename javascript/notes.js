b3db3cbfeebd91550f99398e71e7d2476f35a5b6
Reuben Brandt <Reubennbrandt@gmail.com>
2016-07-05 13:12:40 -0700

    Add add note function

--- javascript/notes.js
+++ javascript/notes.js
@@ -6,12 +6,11 @@ var Note = React.createClass({
       this.setState({editing: true});
     },
     save: function(){
-      var val = this.refs.newText.getDOMNode().value;
-      alert("Save " + val );
+      this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
       this.setState({editing: false})
     },
     remove: function(){
-      alert('removing note');
+      this.props.onRemove(this.props.index);
     },
     renderDisplay: function(){
       return (
@@ -58,18 +57,41 @@ var Board = React.createClass({
   },
   getInitialState: function() {
     return {
-      notes: [
-      "Finish the project"]
+      notes: []
     };
   },
+  add: function(text){
+    var arr=this.state.notes;
+    arr.push(text);
+    this.setState({notes: arr})
+  },
+  update: function(newText, i){
+    var arr= this.state.notes;
+    arr[i] = newText;
+    this.setState({notes: arr});
+  },
+  remove: function(i){
+    var arr= this.state.notes;
+    arr.splice(i, 1);
+    this.setState({notes:arr})
+  },
+  eachNote: function(note, i){
+    return(
+      <Note key={i}
+        index={i}
+        onChange={this.update}
+        onRemove={this.remove}
+        >{note}
+      </Note>
+    );
+  },
   render: function() {
-    return <div className="board">
-      {this.state.notes.map(function(note, i){
-        return (
-          <Note key={i}>{note}</Note>
-        );
-      })}
+    return (<div className="board">
+      {this.state.notes.map(this.eachNote)}
+      <button className="btn btn-sm glyphicon glyphicon-plus"
+      onClick={this.add}/>
     </div>
+    );
   }
 });
 React.render(<Board count={10}/>,
