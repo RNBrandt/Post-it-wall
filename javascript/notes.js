3390232cc5ca631cd8e067ef5e3b44e98829b2ee
Reuben Brandt <Reubennbrandt@gmail.com>
2016-07-05 14:01:39 -0700

    Give notes a natural look

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,57 +1,67 @@
 var Note = React.createClass({
-   getInitialState: function(){
+  getInitialState: function() {
     return {editing: false}
-   },
-    edit: function(){
-      this.setState({editing: true});
-    },
-    save: function(){
-      this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
-      this.setState({editing: false})
-    },
-    remove: function(){
-      this.props.onRemove(this.props.index);
-    },
-    renderDisplay: function(){
-      return (
-        <div className="note">
-          <p>{this.props.children}</p>
-          <span>
-            <button onClick={this.edit}
-             className="btn btn-primary glyphicon-pencil"/>
-            <button onClick={this.remove}
-            className="btn btn-danger glyphicon-trash"/>
-          </span>
-        </div>
-        );
-    },
-    renderForm: function(){
-      return (
-        <div className ="note">
-        <textarea ref="newText" defaultValue={this.props.children}
-        className="form-control"></textarea>
-        <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disc"/>
-        </div>
-        )
-    },
-    render: function() {
-      if (this.state.editing){
+  },
+  componentWillMount: function() {
+    this.style = {
+      right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+      top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+      transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+    };
+  },
+  randomBetween: function(min, max) {
+    return (min + Math.ceil(Math.random() * max));
+  },
+  edit: function() {
+    this.setState({editing: true});
+  },
+  save: function() {
+    this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
+    this.setState({editing: false});
+  },
+  remove: function() {
+    this.props.onRemove(this.props.index);
+  },
+  renderDisplay: function() {
+    return (
+      <div className="note" style={this.style}>
+        <p>{this.props.children}</p>
+        <span>
+          <button onClick={this.edit}
+                  className="btn btn-primary glyphicon glyphicon-pencil"/>
+          <button onClick={this.remove}
+                  className="btn btn-danger glyphicon glyphicon-trash"/>
+        </span>
+      </div>
+    );
+  },
+  renderForm: function() {
+    return (
+      <div className="note" style={this.style}>
+      <textarea ref="newText" defaultValue={this.props.children}
+      className="form-control"></textarea>
+      <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+      </div>
+    )
+  },
+  render: function() {
+    if (this.state.editing) {
         return this.renderForm();
-      }
-      else {
-        return this.renderDisplay();
-      }
     }
+    else {
+        return this.renderDisplay();
+    }
+  }
 });
 
 var Board = React.createClass({
   propTypes: {
     count: function(props, propName) {
       if (typeof props[propName] !== "number"){
-        return new Error('Count must be a number');
+          return new Error('The count property must be a number');
       }
-      if (props[propName] > 100){
-        return new Error('Creating ' + props[propName] + "notes is too many")
+      if (props[propName] > 100) {
+          return new Error("Creating " + props[propName] + " notes is ridiculous");
       }
     }
   },
@@ -60,39 +70,59 @@ var Board = React.createClass({
       notes: []
     };
   },
-  add: function(text){
-    var arr=this.state.notes;
-    arr.push(text);
-    this.setState({notes: arr})
+  nextId: function() {
+    this.uniqueId = this.uniqueId || 0;
+    return this.uniqueId++;
   },
-  update: function(newText, i){
-    var arr= this.state.notes;
-    arr[i] = newText;
+  componentWillMount: function() {
+    var self = this;
+  },
+  add: function(text) {
+    var arr = this.state.notes;
+    arr.push({
+        id: this.nextId(),
+        note: text
+    });
     this.setState({notes: arr});
   },
-  remove: function(i){
-    var arr= this.state.notes;
-    arr.splice(i, 1);
-    this.setState({notes:arr})
+  update: function(newText, i) {
+    var arr = this.state.notes;
+    arr[i].note = newText;
+    this.setState({notes:arr});
   },
-  eachNote: function(note, i){
-    return(
-      <Note key={i}
+  remove: function(i) {
+    var arr = this.state.notes;
+    arr.splice(i, 1);
+    this.setState({notes: arr});
+  },
+  eachNote: function(note, i) {
+    return (
+      <Note key={note.id}
         index={i}
         onChange={this.update}
         onRemove={this.remove}
-        >{note}
-      </Note>
+      >{note.note}</Note>
     );
   },
   render: function() {
     return (<div className="board">
       {this.state.notes.map(this.eachNote)}
-      <button className="btn btn-sm glyphicon glyphicon-plus"
-      onClick={this.add}/>
+      <button className="btn btn-sm btn-success glyphicon glyphicon-plus"
+        onClick={this.add.bind(null, "New Note")}/>
     </div>
     );
   }
 });
-React.render(<Board count={10}/>,
+
+React.render(<Board count={1}/>,
     document.getElementById('react-container'));
+
+
+
+
+
+
+
+
+
+
